2bd0dbdc3f59592519ce48028b1ab919919cb821
MuhammedSammir7 <mmss2012377@gmail.com>
2024-08-11 19:41:30 +0300

    creating two views and putting them in tabbar and fix the constraints for them and creating a custom cell for the collectionView and add a dummy data to test the design

---
+++ SportsApp/SportsCell.swift
@@ -0,0 +1,14 @@
+//
+//  SportsCell.swift
+//  SportsApp
+//
+//  Created by ios on 11/08/2024.
+//
+
+import UIKit
+
+class SportsCell: UICollectionViewCell {
+    @IBOutlet weak var sportName: UILabel!
+    @IBOutlet weak var sportImage: UIImageView!
+    
+}
--- SportsApp/ViewController.swift
+++ SportsApp/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
 
 }
 
+extension ViewController : UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        4
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sportCell", for: indexPath) as! SportsCell
+        cell.sportName.text = "FootBall"
+        cell.sportImage.image = UIImage(named: "football")
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.width * 0.49, height: self.view.frame.height * 0.15)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        0.1
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let numberOfItemsPerRow: CGFloat = 2
+//        let spacing: CGFloat = 0 // Adjust this if you want any spacing between cells
+//        let totalSpacing = spacing * (numberOfItemsPerRow - 1)
+//        let width = (collectionView.bounds.width - totalSpacing) / numberOfItemsPerRow
+//
+//        return CGSize(width: width, height: width) // Square cell
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 4 // Adjust this value to your liking, or set it to 0 for no space between rows
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 4 // Adjust this value to your liking, or set it to 0 for no space between items
+//    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0) // No padding around the section
+//    }
+}
