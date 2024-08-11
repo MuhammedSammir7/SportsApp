6866a85f43ad74465a2af966b1b32ee84002740e
MuhammedSammir7 <mmss2012377@gmail.com>
2024-08-11 17:02:44 +0300

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by ios on 11/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
